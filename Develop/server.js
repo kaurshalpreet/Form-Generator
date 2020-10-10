70abaf10a648ddaf52232dda1b79b4b857ee7f5b
Ian A <iastesana@gmail.com>
2020-10-10 10:31:45 -0700

    Created backend code, to create the client.

--- Develop/server.js
+++ Develop/server.js
@@ -29,18 +29,18 @@ db.sequelize.sync().then(function() {
   });
 });
 
-var exphbs = require("express-handlebars");
+// var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers-controller.js");
+// // Import routes and give the server access to them.
+// var routes = require("./controllers/burgers-controller.js");
 
-app.use(routes);
+// app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+// // Start our server so that it can begin listening to client requests.
+// app.listen(PORT, function() {
+//   // Log (server-side) when our server has started
+//   console.log("Server listening on: http://localhost:" + PORT);
+// });
