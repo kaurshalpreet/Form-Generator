28c0c0eca005bf369d8f63f0a005aedc17684d33
Shalpreet Kaur <kaurshalpreet@gmail.com>
2020-10-13 19:00:18 -0700

    Updated Database

--- Develop/models/member.js
+++ Develop/models/member.js
@@ -4,103 +4,127 @@ module.exports = function (sequelize, DataTypes) {
     name: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     m_initial: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     last_name: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     ss_number: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
+    },
+
+    d_o_b: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: false,
     },
 
     spouse_first_name: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     spouse_middle_initial: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     spouse_last_name: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     spouse_ss_number: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
+    },
+
+    spouse_d_o_b: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: false,
     },
 
     address_one: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     address_two: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     apartment_number: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     city: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     state: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     zip: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     foreign_country_name: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     foreign_province_state: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
     },
 
     foreign_postal_code: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: true,
-      unique: true,
+      unique: false,
+    },
+
+    phone_number: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: false,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: false,
     },
     
   });
